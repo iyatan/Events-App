2f30028b43594817850be1ee3e9beb62b4b897b7
iyatan <iyatan.atchoro@mail.mcgill.ca>
2019-05-24 21:32:02 -0400

    fixing issues

---
+++ src/components/NavBar.vue
@@ -0,0 +1,18 @@
+<template>
+    <div id="nav" class="nav">
+        <router-link to="/" class="brand">Real World Events</router-link>
+        <nav>
+            <router-link :to="{name: 'event-list'}">List</router-link> |
+            <router-link :to="{name: 'event-create'}">Create</router-link>|
+
+        </nav>
+      
+    </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
--- src/router.js
+++ src/router.js
@@ -1,8 +1,8 @@
 import Vue from "vue";
 import Router from "vue-router";
-import EventCreate from "./views/EventCreate";
-import EventList from "./views/EventList";
-import EventShow from "./views/EventShow";
+import EventCreate from "./views/EventCreate.vue";
+import EventList from "./views/EventList.vue";
+import EventShow from "./views/EventShow.vue";
 
 Vue.use(Router);
 
